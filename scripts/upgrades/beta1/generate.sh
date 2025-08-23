9682c94a304f91723a418f1063041ab9b30a1435
Thomas Haller <thomashaller@gmx.at>
2025-08-23 23:45:51 +0200

    low majority upgrade

--- scripts/upgrades/beta1/generate.sh
+++ scripts/upgrades/beta1/generate.sh
@@ -7,6 +7,6 @@
 output_file="2025_08_04_contracts.txt"
 NETWORK="--network beta1"
 
-# npx hardhat $NETWORK analyze
-#npx hardhat --network beta1 deployLowMajorityContract
-npx hardhat --network beta1 getUpgradeCalldata --output "$output_file" --contract DiamondDao --init-func initializeV2 0x67946D41eafeb42873cB72e21e2f386e6F3c13f3 
+#npx hardhat $NETWORK analyze
+#npx hardhat $NETWORK deployLowMajorityContract
+npx hardhat --network beta1 getUpgradeCalldata --output "$output_file" --contract DiamondDao --init-func initializeV2 0xf30214ee3Be547E2E5AaaF9B9b6bea26f1Beca37 
